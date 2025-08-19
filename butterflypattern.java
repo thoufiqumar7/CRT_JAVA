e3b979a080dfb2b29f2a934357a29333bdf9aa04
thoufiqumar7 <thoufiqumar777@gmail.com>
2025-08-19 14:52:56 +0530

    Create butterflypattern.java

---
+++ butterflypattern.java
@@ -0,0 +1,49 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= 8; j++) {
+                System.out.print(" ");
+            }
+            for (int z = 1; z <= i; z++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int j = 1; j <= 3; j++) {
+            System.out.print(" ");
+        }
+        for (int z = 1; z <= 4; z++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int j = 1; j <= 8; j++) {
+            System.out.print(" ");
+        }
+        for (int z = 1; z <= 3; z++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int j = 1; j <= 8; j++) {
+            System.out.print(" ");
+        }
+        for (int z = 1; z <= 3; z++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int j = 1; j <= 8; j++) {
+            System.out.print(" ");
+        }
+        for (int z = 1; z <= 2; z++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int j = 1; j <= 8; j++) {
+            System.out.print(" ");
+        }
+        for (int z = 1; z <= 1; z++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
